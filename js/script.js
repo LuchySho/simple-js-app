83a7c611902c4d3b2c4e63416775d43b3035f711
Oluchi Sodeinde <oluchined@gmail.com>
2020-08-13 01:14:43 +0100

    Pokemon List creation
    Listed Pokemon names, height and type

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var pokemonList = [];
+pokemonList = [
+  {
+    creatureName:'Bulbasaur', 
+    creatureHeight: 7, 
+    creatureType: ['grass', 'poison']
+  },
+  {
+    creatureName: 'Charizard', 
+    creatureHeight: 17, 
+    creatureType: ['fire', 'flying']
+  },
+  {
+    creatureName: 'Beedrill', 
+    creatureHeight: 10, 
+    creatureType: ['bug', 'poison']
+  },
+  {
+    creatureName: 'Exeggutor', 
+    creatureHeight: 20, 
+    creatureType: ['psychic', 'grass']
+  }
+]
+console.log(pokemonList)
